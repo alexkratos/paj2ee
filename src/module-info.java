c0f2d712adabf53ff752959a6b38602c8ec1a9bc
alexkratos <alex.kratos08@gmail.com>
2021-01-13 16:55:56 +0200

    back to beginings

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,5 @@
 public class GitTestApp {
  public static void main(String[] args) {
  System.out.println("Hello Git, from branch bug#12345 !!!!!!!");
- System.out.println("Hello Git, from branch bug#12345 !!!!!!!");
  }
 }
